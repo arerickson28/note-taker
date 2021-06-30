045487c236bd7d62ea4dfa7c7002625ed37aa6a7
Austen Rhyce Erickson <a.r.erickson.28@gmail.com>
2021-06-30 09:13:37 -0500

    added api delete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,14 @@
 const fs = require("fs");
 const { v4: uuidv4 } = require('uuid');
 
+function remove(array, key, value) {
+    const index = array.findIndex(obj => obj[key] === value);
+    return index >= 0 ? [
+        ...array.slice(0, index),
+        ...array.slice(index + 1)
+    ] : array;
+};
+
 
 module.exports = (app) => {
 
@@ -12,10 +20,18 @@ module.exports = (app) => {
     app.post('/api/notes', (req, res) => {
         const db = JSON.parse(fs.readFileSync("db/db.json", "utf-8"));
         console.log(req.body) ;
-        let note = req.body;
+        const note = req.body;
         note.id = uuidv4();
         db.push(note);
         fs.writeFileSync("db/db.json", JSON.stringify(db));
         res.json(note);
+    });
+
+    app.delete('/api/notes/:id', (req, res ) => {
+        const db = JSON.parse(fs.readFileSync("db/db.json", "utf-8"));
+        const delNote = req.params.id ;
+        const newDB = remove(db, "id", delNote);
+        fs.writeFileSync("db/db.json", JSON.stringify(newDB));
+        res.json(newDB);
     });
 }
